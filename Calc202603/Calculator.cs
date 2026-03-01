7e5b0424df27abb36884545cc9a0d1f60bfe40e7
anastasiiaskub <anastasiia.skub.25@kse.org.ua>
2026-03-01 23:27:20 +0200

    make clearInput

--- Calc202603/Calculator.cs
+++ Calc202603/Calculator.cs
@@ -4,6 +4,28 @@ public class Calculator
 {
     public void doCalc(string input, out float result)
     {
+        string clearInput ="";
+        foreach (var letter in input)
+        {
+            if (letter == ' ')
+            {
+                clearInput = clearInput;
+            }
+            else if (letter == ',')
+            {
+                clearInput += ".";
+            }
+            else
+            {
+                clearInput += letter;
+            }
+        }
+            
+        
+        
+        
+        Console.WriteLine($"clearInput = {clearInput}");
         result = 1;
+        
     }
 }
