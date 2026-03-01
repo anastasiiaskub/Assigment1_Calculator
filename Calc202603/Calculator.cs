3efdd00c22bdf73b3cbc9d479c1f0e43de8f5417
anastasiiaskub <anastasiia.skub.25@kse.org.ua>
2026-03-01 22:55:37 +0200

    begin

---
+++ Calc202603/Calculator.cs
@@ -0,0 +1,9 @@
+namespace Calc202603;
+
+public class Calculator
+{
+    public void doCalc(string input, out float result)
+    {
+        result = 1;
+    }
+}
---
+++ Calc202603/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calc202603;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write what do you want to calc");
+        string pp = Console.ReadLine();
+        float result = 0;
+        Calculator dapofig = new Calculator();
+        dapofig.doCalc(pp, out result);
+        Console.WriteLine($"result: {result}");
+    }
+}
