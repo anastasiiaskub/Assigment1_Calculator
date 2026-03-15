0f9acda352e607ae207bd70a431d0d6e9f8497f7
anastasiiaskub <anastasiia.skub.25@kse.org.ua>
2026-03-15 22:29:00 +0200

    done

--- Calc202603/Program.cs
+++ Calc202603/Program.cs
@@ -4,12 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        string Linput = "-7+4*(2-1)";
-        string lOutput = "";
+        // string Linput = "-7+4*(2-1)";
         
         Console.WriteLine("Write what do you want to calc");
-        // string equation = Console.ReadLine();
-        string equation = Linput;
+        string equation = Console.ReadLine();
+        // string equation = Linput;
         double result = 0;
         Console.WriteLine($"User's input:{equation}");
         Calculator calc1 = new Calculator();
